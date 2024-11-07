10abeda6d78d67a25255913f788935b984781a65
Tamo <tamuna.kakhidze.1@btu.edu.ge>
2024-11-07 17:54:31 +0400

    Refactor ForgotPasswordViewController and viewmodel

--- BikeRentalApp/Scenes/ForgotPasswordPage/ViewModel/ForgotPasswordViewModel.swift
+++ BikeRentalApp/Scenes/ForgotPasswordPage/ViewModel/ForgotPasswordViewModel.swift
@@ -8,14 +8,21 @@
 import Foundation
 import FirebaseAuth
 
+// MARK: - Protocols
+
 protocol ForgotPasswordViewModelDelegate: AnyObject {
     func didSendPasswordResetEmail(success: Bool, error: String?)
 }
 
+// MARK: - ForgotPasswordViewModel
+
 final class ForgotPasswordViewModel {
+    
+    // MARK: - Properties
     
     weak var delegate: ForgotPasswordViewModelDelegate?
     
+    // MARK: - Methods
     func resetPassword(email: String) {
         AuthService.shared.forgotPassword(with: email) { [weak self] error in
             if let error = error {
