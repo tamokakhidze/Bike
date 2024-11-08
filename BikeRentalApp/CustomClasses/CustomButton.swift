78aa8ed6d7a32ce1a562501e231595b4ea8a2010
Tamo <tamuna.kakhidze.1@btu.edu.ge>
2024-11-08 16:36:04 +0400

    Refactor CustomButton Class

--- BikeRentalApp/CustomClasses/CustomButton.swift
+++ BikeRentalApp/CustomClasses/CustomButton.swift
@@ -7,28 +7,45 @@
 
 import UIKit
 
+// MARK: - CustomButton
+
 class CustomButton: UIButton {
+    
+    // MARK: - Init
     
     init(title: String, hasBackground: Bool = false, width: CGFloat) {
         super.init(frame: .zero)
-        self.setTitle(title, for: .normal)
-        self.backgroundColor = hasBackground ? .black: .clear
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.heightAnchor.constraint(equalToConstant: hasBackground ? 50 : 25).isActive = true
-        self.widthAnchor.constraint(equalToConstant: width).isActive = true
-        self.layer.cornerRadius = 25
-        self.titleLabel?.font = .systemFont(ofSize: hasBackground ? 14 : 12, weight: .bold)
-        self.setTitleColor(hasBackground ? .white : .systemBlue, for: .normal)
-        self.setTitleColor(.systemGray, for: .selected)
-        let color = hasBackground ? UIColor.highlightedButton : UIColor.clear
-        self.setBackgroundImage(image(withColor: color, size: CGSize(width: 1, height: 1)), for: .highlighted)
-        self.contentHorizontalAlignment = .center
-        self.contentVerticalAlignment = .center
-        self.clipsToBounds = true
+        configureTitle(title, hasBackground: hasBackground)
+        configureAppearance(hasBackground: hasBackground, width: width)
+        configureSelectedState(hasBackground: hasBackground)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configurations
+    
+    private func configureTitle(_ title: String, hasBackground: Bool) {
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = .systemFont(ofSize: hasBackground ? Sizing.titleFontLarge : Sizing.titleFontSmall, weight: .bold)
+        self.setTitleColor(hasBackground ? Colors.textOnBackground : Colors.primaryText, for: .normal)
+        self.setTitleColor(Colors.disabledText, for: .selected)
+    }
+    
+    private func configureAppearance(hasBackground: Bool, width: CGFloat) {
+        self.backgroundColor = hasBackground ? Colors.background: .clear
+        self.heightAnchor.constraint(equalToConstant: hasBackground ? Sizing.largeHeight : Sizing.smallHeight).isActive = true
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        self.layer.cornerRadius = Sizing.cornerRadius
+        self.clipsToBounds = true
+        self.contentHorizontalAlignment = .center
+        self.contentVerticalAlignment = .center
+    }
+    
+    private func configureSelectedState(hasBackground: Bool) {
+        let color = hasBackground ? UIColor.highlightedButton : .clear
+        self.setBackgroundImage(image(withColor: color, size: CGSize(width: 1, height: 1)), for: .highlighted)
     }
     
     private func image(withColor color: UIColor, size: CGSize) -> UIImage {
@@ -39,5 +56,25 @@ class CustomButton: UIButton {
         let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
         UIGraphicsEndImageContext()
         return image
+    }
+}
+
+// MARK: - Constants Extension
+
+extension CustomButton {
+    enum Sizing {
+        static let cornerRadius: CGFloat = 25
+        static let largeHeight: CGFloat = 50
+        static let smallHeight: CGFloat = 25
+        static let titleFontLarge: CGFloat = 14
+        static let titleFontSmall: CGFloat = 12
+    }
+    
+    enum Colors {
+        static let background = UIColor.black
+        static let highlightedBackground = UIColor.highlightedButton
+        static let textOnBackground = UIColor.white
+        static let primaryText = UIColor.systemBlue
+        static let disabledText = UIColor.systemGray
     }
 }
